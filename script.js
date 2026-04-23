d19ba0985a3646a0fc66057b907646cba947fdab
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-23 18:35:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Person(name, age) {
 		this.age = age;
 }
 Person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
 
 function Employee (name, age, jobTitle) {
@@ -13,7 +13,7 @@ function Employee (name, age, jobTitle) {
 }
 Employee.prototype.__proto__ = Person.prototype;
 Employee.prototype.jobGreet = function(){
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	 console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 const person = new Person("Alice", 25);
 person.greet();
